dea2c9b9f0730be9d20fb97916d3d0fdb0afe26f
Mehwish Hasny <mehwish.hasny@gmail.com>
2024-11-01 19:52:35 +0500

    commit added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -74,7 +74,6 @@ Moreover, their financial experts work closely with each client, ensuring that e
 <p className="text-black font-bold text-center p-4">Elite Estates provides an integrated portfolio of services, allowing you to access everything you need in one place. From real estate investment to property management, they streamline the decision-making process and simplify your financial journey.</p>
 <h2 className="text-gray-600 font-bold text-xl mb-4">Bespoke Solutions</h2> 
 <p className="text-black font-bold text-center p-4">No two clients are alike and neither are their financial needs. Elite Estates takes the time to understand each individuals circumstances, crafting unique strategies that are perfectly aligned with your goals.</p>
-</p>
 
 <h2 className="text-gray-600 font-bold text-xl mb-4">A Foundation of Trust</h2> 
 <p className="text-black font-bold text-center p-4">Built on transparency and ethical practices Elite Estates has cultivated lasting relationships with clients who value integrity and accountability. Their client-focused approach ensures that you receive advice and recommendations rooted in trust.</p>
